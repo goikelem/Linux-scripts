b1dc89bf3e6728c09cd206cb620fc8458d03f278
goitom kelem <goikelem@gmail.com>
2025-01-06 21:38:31 -0800

    Update Download_IDVI.sh

--- Download_IDVI.sh
+++ Download_IDVI.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
-# download NDVI copernicus data 
-#
-DT=`date +%Y`
-for mm in {01..12..1}; do
-for DK in 10, 20, 29, 28, 30, 31; do 
-#
-cd /home/${USER}/DATA/NDVI
-#
-wget -r --reject "index.html*" --user=goitomk --password="puthereyour password" https://land.copernicus.vgt.vito.be/PDF/datapool/Vegetation/Indicators/NDVI_300m_V2/${DT}/${mm}/${DK}/?coord=22.50,-12.60,51.52,23.27
-# put your password with out double quotes  e.g password=password
-cd /home/${USER}/DATA/NDVI/land.copernicus.vgt.vito.be/PDF/datapool/Vegetation/Indicators/NDVI_300m_V2/${DT}/${mm}/${DK}/NDVI300_${DT}${mm}${DK}0000_GLOBE_OLCI_V2.0.1
-# 
-sleep 2
-done 
-done 
-rm -rf /home/${USER}/DATA/NDVI/land.copernicus.vgt.vito.be/PDF/image
-# 
-rm -rf *.xml
-# rm -rf *.tiff 
-# 
-cdo sellonlatbox,20,52,-12,24 c_gls_NDVI300_202109110000_GLOBE_OLCI_V2.0.1.nc NDVI_${DT}${mm}${DK}_EA_OLCI.nc
+# Download and process NDVI Copernicus data
+
+# Set the current year
+DT=$(date +%Y)
+
+# Base directory for data
+BASE_DIR="/home/${USER}/DATA/NDVI"
+COPERNICUS_URL="https://land.copernicus.vgt.vito.be/PDF/datapool/Vegetation/Indicators/NDVI_300m_V2"
+
+# Loop through months and dekads (10-day periods)
+for mm in {01..12}; do
+  for DK in 10 20 29 28 30 31; do
+    # Navigate to the base directory
+    cd "${BASE_DIR}" || exit
+
+    # Construct the download URL and destination directory
+    DOWNLOAD_URL="${COPERNICUS_URL}/${DT}/${mm}/${DK}/?coord=22.50,-12.60,51.52,23.27"
+    DEST_DIR="${BASE_DIR}/land.copernicus.vgt.vito.be/PDF/datapool/Vegetation/Indicators/NDVI_300m_V2/${DT}/${mm}/${DK}/NDVI300_${DT}${mm}${DK}0000_GLOBE_OLCI_V2.0.1"
+
+    # Download data using wget
+    wget -r --reject "index.html*" --user=goitomk --password="your_password" "${DOWNLOAD_URL}"
+
+    # Change to the destination directory
+    cd "${DEST_DIR}" || exit
+
+    # Pause for 2 seconds
+    sleep 2
+  done
+done
+
+# Clean up unnecessary files
+rm -rf "${BASE_DIR}/land.copernicus.vgt.vito.be/PDF/image"
+rm -rf "${BASE_DIR}"/*.xml
+
+# Process the data with cdo (adjust the file names and parameters as necessary)
+cdo sellonlatbox,20,52,-12,24 "c_gls_NDVI300_202109110000_GLOBE_OLCI_V2.0.1.nc" "NDVI_${DT}${mm}${DK}_EA_OLCI.nc"
